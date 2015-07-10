5cd0f50c1c3f57530cfde7d6a3ba33f05d8b8c79
Duffed <duffed90@gmail.com>
2015-07-10 22:21:12 +0200

    Kontaktformular
    sendet vorerst zu duffed90@gmail.com

--- kontakt.php
+++ kontakt.php
@@ -29,36 +29,57 @@
 			<iframe class="u-full-width" id="googlemaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1283.7648537113694!2d11.57923139882384!3d49.945165178171436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47a1a2c5532c0c5b%3A0x6800e73e6fd406d0!2sW%C3%B6lfelstra%C3%9Fe+6%2C+95444+Bayreuth!5e0!3m2!1sde!2sde!4v1435954462821" allowfullscreen draggable="false"></iframe>
 	
 
-			<section id="intro_kontaktformular" class="row">
-				<h5>Kontaktformular</h5>
-				<hr>
-				<i class="fa fa-chevron-down"></i>
-			</section>
 
-			<!-- Kontaktformular -->
-			<form id="kontaktformular">
-		
-				<label for="exampleEmailInput">Email-Adresse</label>
-				<input class="u-full-width" type="email" id="e_Email">
-				<label for="betreff">Betreff</label>
-				<input class="u-full-width" type="text" id="e_Betreff">
-				<label for="exampleMessage">Nachricht</label>
-				<textarea class="u-full-width" placeholder="Ihre Nachricht an uns" id="e_Nachricht"></textarea>
 
-				<?php 
-					$zahl1 = rand(10,20);
-					$zahl2 = rand(1,9);
-					$ergebnis = $zahl1+$zahl2;
-				?>
+			<?php
+				if (!isset($_POST["e_Senden"]))    /* display the contact form */
+				{?>
+					<?php include("include/kontaktformular.php") ?>
+			<?php
+				} 
+				else /* send the submitted data */
+				{
+					$name=$_POST['Name'];
+					$email=$_POST['Email'];
+					$betreff=$_POST['Betreff'];
+					$nachricht=$_POST['Nachricht'];
+					$captcha=$_POST['captcha'];
+					$e_captcha=$_POST['e_captcha'];
 
-				<label for="captcha"><?php echo $zahl1." + ".$zahl2." = "?></label>
-				<input type="text" class="u-full-width">
-				<label class="example-send-yourself-copy">
-					<input type="checkbox">
-					<span class="label-body">Kopie an mich selbst schicken</span>
-				</label>
-				<input class="button-primary" type="submit" id="e_Senden" value="Senden">
-			</form>
+					//Check if everything is filled
+					$fields = array('Name', 'Email', 'Betreff', 'Nachricht');
+
+					echo "<section class='kontaktformular row'>";
+					$error = false; //No errors yet
+					foreach($fields AS $fieldname) { //Loop trough each field
+						if(!isset($_POST[$fieldname]) || empty($_POST[$fieldname])) {
+							echo '<p><center>Feld '.$fieldname.' fehlt!</center></p>'; //Display error with field
+							$error = true; 
+						}
+					}
+
+					if(!$error) { //Only create queries when no error occurs
+						//check captcha
+						if ($captcha != $e_captcha){
+							echo "Captcha falsch eingegeben, bitte erneut versuchen.";
+						} else {
+							//send email
+							$from="Von: $name<$email>\r\nReturn-path: $email";
+							$subject='Greim Kontaktformular: "'.$betreff.'" (Von '.$name.' - '.$email.')';
+							mail("duffed90@gmail.com", $subject, $nachricht, $from);
+
+							//mail on myself
+							if ($_POST['e_captcha']) {
+								mail($email, $subject, $nachricht, $from);
+							}
+							echo "<h5>E-Mail wurde versendet.</h5>";
+						}
+					}
+					echo "</section>";
+
+					include("include/kontaktformular.php");
+				}
+			?>
 
 		</div>
 	</main>
